7fc465c9132286b68eba9b438c6787e4f9f94de2
Artem Popov <artfwo@gmail.com>
2019-10-06 19:29:16 +0200

    update python examples

--- docs/grid-studies/python/files/grid-studies-1.py
+++ docs/grid-studies/python/files/grid-studies-1.py
@@ -3,17 +3,24 @@
 import asyncio
 import monome
 
-class GridStudies(monome.App):
+class GridStudies(monome.GridApp):
     def __init__(self):
-        super().__init__('/monome')
+        super().__init__()
 
     def on_grid_key(self, x, y, s):
         print("key:", x, y, s)
         self.grid.led_level_set(x, y, s*15)
 
 if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
     grid_studies = GridStudies()
 
-    loop = asyncio.get_event_loop()
-    asyncio.async(monome.SerialOsc.create(loop=loop, autoconnect_app=grid_studies))
+    def serialosc_device_added(id, type, port):
+        print('connecting to {} ({})'.format(id, type))
+        asyncio.ensure_future(grid_studies.grid.connect('127.0.0.1', port))
+
+    serialosc = monome.SerialOsc()
+    serialosc.device_added_event.add_handler(serialosc_device_added)
+
+    loop.run_until_complete(serialosc.connect())
     loop.run_forever()
